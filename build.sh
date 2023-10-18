f235491d9ff2ee000db360f47f289e914cd611e8
Michael Cardell Widerkrantz <mc@tillitis.se>
2023-10-17 19:10:05 -0700

    Make build.sh a little more resiliant

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
 #! /bin/sh
 
-git clone -b v0.0.1 https://github.com/tillitis/tkey-libs.git ../tkey-libs
-git clone -b v0.0.7 https://github.com/tillitis/tkey-device-signer.git ../tkey-device-signer
+tkey_libs_version="v0.0.1"
+signer_version="v0.0.7"
+
+printf "Building tkey-libs with version: %s\n" "$tkey_libs_version"
+printf "Building signer with version: %s\n" "$signer_version"
+
+if [ -d ../tkey-libs ]
+then
+    (cd ../tkey-libs; git checkout "$tkey_libs_version")
+else
+    git clone -b "$tkey_libs_version" https://github.com/tillitis/tkey-libs.git ../tkey-libs
+fi
+
+if [ -d ../tkey-device-signer ]
+then
+    (cd ../tkey-device-signer; git checkout "$signer_version")
+else
+    git clone -b "$signer_version" https://github.com/tillitis/tkey-device-signer.git ../tkey-device-signer
+fi
 
 make -j -C ../tkey-libs
 make -j -C ../tkey-device-signer
